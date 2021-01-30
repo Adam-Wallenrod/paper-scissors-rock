9c6ef6b9a311aa45f8a3f8108e4222f3298de618
Adam-Wallenrod <psredniawa@gmail.com>
2021-01-30 18:15:39 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'papper-scissors-rock-app';
+}
